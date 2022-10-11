feb6d8d69cb73795083f3c48bdeef320322df103
Danylo Nikora <d.nikora2019@gmail.com>
2022-10-11 12:31:25 +0300

    Add getAllNonEmptySchedules service function

--- src/services/class.js
+++ src/services/class.js
@@ -29,3 +29,19 @@ export async function getNextClass(date, subgroup) {
   ]);
   return nextClasses[0];
 }
+
+export async function getAllNonEmptySchedules(date, subgroup) {
+  const allNonEmptySchedules = await Class.aggregate([
+    {
+      $match: {
+        subgroup: { $in: [0, subgroup] },
+        start: { $gt: date },
+      },
+    },
+    { $sort: { start: 1 } },
+    { $group: { _id: "$date", classes: { $push: "$$ROOT" } } },
+    { $sort: { _id: 1 } },
+    { $project: { _id: 0, date: "$_id", classes: 1 } },
+  ]);
+  return allNonEmptySchedules;
+}
