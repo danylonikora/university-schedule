2dcb1f401c85bebb74b402e8d000535d7f8d3f0a
Danylo Nikora <d.nikora2019@gmail.com>
2022-10-19 13:08:23 +0300

    Update description message in registerUser controller

--- src/controllers/registerUser.js
+++ src/controllers/registerUser.js
@@ -2,6 +2,13 @@ import { createUser } from "../services/user.js";
 import getCallbackData from "../utils/getCallbackData.js";
 
 export default async function registerUserController(bot, msg) {
+  let html = "";
+  html += "Выбери номер своей подгруппы";
+  html += "\n";
+  html += "1 - бизнес планирование";
+  html += "\n";
+  html += "2 - интернет технологии";
+
   const subgroupInlineButtons = [
     [
       { text: "1", callback_data: "1" },
@@ -12,9 +19,10 @@ export default async function registerUserController(bot, msg) {
   const userSubgroup = await getCallbackData(
     bot,
     msg.chat.id,
-    "Выбери номер своей подгруппы",
+    html,
     subgroupInlineButtons,
-    1000 * 60 * 5
+    1000 * 60 * 5,
+    { parse_mode: "HTML" }
   );
 
   return createUser({
